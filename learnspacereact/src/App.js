db8415dfbeedcf8ee7cefacacd6596609005db6e
Unattched <62789456+Unattched@users.noreply.github.com>
2021-05-30 06:09:46 +0400

    kid fix

--- learnspacereact/src/App.js
+++ learnspacereact/src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import {Route, Router, Switch} from "react-router";
+import {Route, Router, Switch} from "react-router-dom";
 import LoginPage from "./LoginPage.js"
 import {useState} from "react";
+import firebase from "firebase";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <div className="App">
-        {/*<Router>*/}
-        {/*<Switch>*/}
-        {/*  <Route exact={true} path={"/"}>*/}
-        {/*      <LoginPage/>*/}
-        {/*  </Route>*/}
-        {/*</Switch>*/}
-        {/*</Router>*/}
-      <button onClick={() => {
-        let newCount = count + 1
-        setCount(newCount)
-      }}>Please click me!</button>
-      {count}
+        <Router>
+            <Switch>
+                <Route exact={true} path={"/"}><LoginPage/></Route>
+                {/*<Route exact path={"/home"}></Route>*/}
+            </Switch>
+        </Router>
     </div>
   );
 }
